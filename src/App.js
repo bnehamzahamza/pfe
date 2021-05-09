42182f4aadf697f249b457b6ecae864f81f49ba7
Benhamza <benhamzahamza70@gmail.com>
2021-05-09 19:57:30 +0200

    finishing demande

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import './App.css'
+import './App.css';
 import passDemande from './Components/passDemande'
+
 
 
 
--- src/Components/passDemande.jsx
+++ src/Components/passDemande.jsx
@@ -1,7 +1,38 @@
 import React, { Component } from 'react'
 export default class passDemande extends Component {
 
+    constructor(props) {
+        super(props)
 
+        this.state = {
+            destinataire : '',
+            lieu : '',
+            degre_urgence : '',
+            description : ''
+
+
+        }
+        this.changeDestinataire = this.changeDestinataire.bind(this);
+        this.changeLieu = this.changeLieu.bind(this);
+        this.changeUrgence = this.changeUrgence.bind(this);
+        this.changeDescription = this.changeDescription.bind(this);
+
+    }
+
+    changeDestinataire= (event) => {
+        this.setState({destinataire: event.target.value});
+    }
+    changeLieu= (event) => {
+        this.setState({lieu: event.target.value});
+    }
+    changeUrgence= (event) => {
+        this.setState({degre_urgence: event.target.value});
+    }
+    changeDescription= (event) => {
+        this.setState({description: event.target.value});
+    }
+
+    
     render() {
         return (
             <div className="container">
@@ -10,18 +41,30 @@ export default class passDemande extends Component {
     <h3 className="text-center">connexion</h3>
 
     <div className="form-group">
-        <label>pseudo</label>
-        <input className="form-control" placeholder="nom" value={this.state.nom} onChange={this.changeNom} />
+        <label>destinataire</label>
+        <input className="form-control" placeholder="destination" value={this.state.destinataire} onChange={this.changeDestinataire} />
     </div>
 
     <div className="form-group">
-        <label>mot de passe</label>
-        <input type="password" className="form-control" placeholder="mdp"
-        value={this.state.mdp}  onChange={this.changeMdp} />
+        <label>lieu</label>
+        <input className="form-control" placeholder="lieu"
+        value={this.state.lieu}  onChange={this.changeLieu} />
+    </div>
+
+    <div className="form-group">
+        <label>dégre d'urgence</label>
+        <input className="form-control" placeholder="urgent!"
+        value={this.state.degre_urgence}  onChange={this.changeUrgence} />
+    </div>
+
+    <div className="form-group">
+        <label>description</label>
+        <input type="text" className="form-control" placeholder="decription"
+        value={this.state.description}  onChange={this.changeDescription} />
     </div>
 
 
-    <button type="submit" className="btn btn-dark btn-lg btn-block" onClick={this.logTest}>Connexion</button>
+    <button type="submit" className="btn btn-dark btn-lg btn-block" onClick={this.saveDemande}>Connexion</button>
     <p className="forgot-password text-right">
     </p>
     </form>
