fabd3cb7f97884b5616217d26456640d710f27b0
Benhamza <benhamzahamza70@gmail.com>
2021-05-09 19:59:59 +0200

    2

--- src/Components/passDemande.jsx
+++ src/Components/passDemande.jsx
@@ -32,43 +32,43 @@ export default class passDemande extends Component {
         this.setState({description: event.target.value});
     }
 
-    
+
     render() {
         return (
             <div className="container">
-            <form id="divLog">
+                        <form id="divLog">
 
-    <h3 className="text-center">connexion</h3>
+                <h3 className="text-center">connexion</h3>
 
-    <div className="form-group">
-        <label>destinataire</label>
-        <input className="form-control" placeholder="destination" value={this.state.destinataire} onChange={this.changeDestinataire} />
-    </div>
+                <div className="form-group">
+                    <label>destinataire</label>
+                    <input className="form-control" placeholder="destination" value={this.state.destinataire} onChange={this.changeDestinataire} />
+                </div>
 
-    <div className="form-group">
-        <label>lieu</label>
-        <input className="form-control" placeholder="lieu"
-        value={this.state.lieu}  onChange={this.changeLieu} />
-    </div>
+                <div className="form-group">
+                    <label>lieu</label>
+                    <input className="form-control" placeholder="lieu"
+                    value={this.state.lieu}  onChange={this.changeLieu} />
+                </div>
 
-    <div className="form-group">
-        <label>dégre d'urgence</label>
-        <input className="form-control" placeholder="urgent!"
-        value={this.state.degre_urgence}  onChange={this.changeUrgence} />
-    </div>
+                <div className="form-group">
+                    <label>dégre d'urgence</label>
+                    <input className="form-control" placeholder="urgent!"
+                    value={this.state.degre_urgence}  onChange={this.changeUrgence} />
+                </div>
 
-    <div className="form-group">
-        <label>description</label>
-        <input type="text" className="form-control" placeholder="decription"
-        value={this.state.description}  onChange={this.changeDescription} />
-    </div>
+                <div className="form-group">
+                    <label>description</label>
+                    <input type="text" className="form-control" placeholder="decription"
+                    value={this.state.description}  onChange={this.changeDescription} />
+                </div>
 
 
-    <button type="submit" className="btn btn-dark btn-lg btn-block" onClick={this.saveDemande}>Connexion</button>
-    <p className="forgot-password text-right">
-    </p>
-    </form>
-</div>
+                <button type="submit" className="btn btn-dark btn-lg btn-block" onClick={this.saveDemande}>Connexion</button>
+                <p className="forgot-password text-right">
+                </p>
+                </form>
+            </div>
         )
     }
 }
